35f0aebff7d5f48a506f323ddc38bacca498bebc
Narendran <narendev0610@gmail.com>
2021-03-03 13:59:22 +0530

    simple event store created to handle missed events, new route is created in event-bus service to get the eventstore data

--- event-bus/index.js
+++ event-bus/index.js
@@ -1,24 +1,30 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const axios = require('axios')
+const axios = require("axios");
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cors);
 
-app.post('/events',(req,res) => {
-    const event = req.body;
+// inmemory event store //! this is for learning only, there are great opensource projects like nats and rabbitmq for production use
+const events = [];
 
-    axios.post('http://localhost:4000/events',event)
-    axios.post('http://localhost:4001/events',event)
-    axios.post('http://localhost:4002/events',event)
-    axios.post('http://localhost:4003/events',event)
-    
+app.post("/events", (req, res) => {
+  const event = req.body;
+  events.push(event);
 
-    res.send({status:'OK'})
-})
+  axios.post("http://localhost:4000/events", event);
+  axios.post("http://localhost:4001/events", event);
+  axios.post("http://localhost:4002/events", event);
+  axios.post("http://localhost:4003/events", event);
 
+  res.send({ status: "OK" });
+});
+
+app.get("/events", (req, res) => {
+  res.send(events);
+});
 
 app.listen(4005, () => {
   console.log("event-bus running on Port 4005");
